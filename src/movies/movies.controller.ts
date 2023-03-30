8277553c9bc9890e1e30c817aa36abbb5f740ab1
mspark <mspark@openerd.com>
2023-03-30 17:16:00 +0900

    feat: post delete patch 작성

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
-import { Param } from '@nestjs/common/decorators';
+import { Controller, Get, Post } from '@nestjs/common';
+import { Delete, Param, Patch } from '@nestjs/common/decorators';
 
 // @Controller()에 있는 'movies가 컨트롤러의 기본 url이 됨
 // 따라서 @Get()을 해도 '/'가 아닌 '/movies'의 경로 지정
@@ -17,5 +17,21 @@ export class MoviesController {
   @Get('/:id')
   getOne(@Param('id') movieId: string) {
     return `This will return one movie with the id: ${movieId}`;
+  }
+
+  // 같은 방식으로 @Post @Delete @Patch 작성
+  @Post()
+  create() {
+    return 'This will create a movie';
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: string) {
+    return `This will delete a movie with the id: ${movieId}`;
+  }
+
+  @Patch('/:id')
+  patch(@Param('id') movieId: string) {
+    return `This will patch a movie with the id: ${movieId}`;
   }
 }
