0ef2c904bf97aa93e77b85463f1efbc13d60b0c6
mspark <mspark@openerd.com>
2023-03-30 18:04:54 +0900

    Movie 형식 지정 후 서비스에 적용

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -49,5 +49,4 @@ export class MoviesController {
       ...updateData,
     };
   }
-
 }
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
 
 @Injectable()
-export class MoviesService {}
+export class MoviesService {
+  // 배열 형식 지정
+  private movies: Movie[] = [];
+
+  getAll(): Movie[] {
+    return this.movies;
+  }
+}
