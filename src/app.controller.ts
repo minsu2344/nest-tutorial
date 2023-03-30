debf6755512ca66a502320cb5164a31377fdb879
mspark <mspark@openerd.com>
2023-03-30 16:18:53 +0900

    feat: 데코레이터 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+// 데코레이터 import 해야 함
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +9,19 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  // @를 붙인 것을 '데코레이터'라고 함(express에서 app.get() 역할)
+  // 데코레이터는 꾸며주는 함수나 클래스와 꼭 붙어있어야 함
+  @Get('/hello')
+  // 여기를 띄우면 데코레이터가 인식을 못함
+  sayHello(): string {
+    return 'Hello everyone';
+  }
+
+  // Post도 가능
+  @Post('/hi')
+  sayHi(): string {
+    return 'hi hi';
   }
 }
