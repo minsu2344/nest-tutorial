712e24d7e141a947f0165dc81c5437dd660ba9b4
mspark <mspark@openerd.com>
2023-03-30 17:10:06 +0900

    feat: 컨트롤러 함수에 매개변수 넣는 방법

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { Param } from '@nestjs/common/decorators';
 
 // @Controller()에 있는 'movies가 컨트롤러의 기본 url이 됨
 // 따라서 @Get()을 해도 '/'가 아닌 '/movies'의 경로 지정
@@ -10,8 +11,11 @@ export class MoviesController {
   }
 
   // params로 id 전달
+  // 함수에 파라미터 넣을 때
+  // @Param()을 이용해 @Get()의 params와 이름 같아야 함(ex: id)
+  // 매개변수는 이름 다르게 해도 무방(movieId)
   @Get('/:id')
-  getOne() {
-    return 'This will return one movie';
+  getOne(@Param('id') movieId: string) {
+    return `This will return one movie with the id: ${movieId}`;
   }
 }
