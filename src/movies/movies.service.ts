d0d857c54bdd46cdc1e29304a82f03213a25fb3b
mspark <mspark@openerd.com>
2023-03-30 18:07:48 +0900

    getAll과 getOne 서비스로 이동

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -6,7 +6,12 @@ export class MoviesService {
   // 배열 형식 지정
   private movies: Movie[] = [];
 
+  // controller에 있던 함수 옮기기
   getAll(): Movie[] {
     return this.movies;
+  }
+
+  getOne(id: string): Movie {
+    return this.movies.find((movie: Movie) => movie.id === +id);
   }
 }
