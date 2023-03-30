943b5859a7e66d9b14995704d4df78cbc0237eb7
mspark <mspark@openerd.com>
2023-03-30 17:00:32 +0900

    feat: params로 id 전달

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
+// @Controller()에 있는 'movies가 컨트롤러의 기본 url이 됨
+// 따라서 @Get()을 해도 '/'가 아닌 '/movies'의 경로 지정
 @Controller('movies')
-export class MoviesController {}
+export class MoviesController {
+  @Get()
+  getAll() {
+    return 'This will return all movies';
+  }
+
+  // params로 id 전달
+  @Get('/:id')
+  getOne() {
+    return 'This will return one movie';
+  }
+}
