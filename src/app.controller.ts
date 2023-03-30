21cd995fdaf129891f383cbb293d4b864b1d3b91
mspark <mspark@openerd.com>
2023-03-30 16:29:14 +0900

    feat: 컨트롤러와 서비스의 역할

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -11,18 +11,24 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  // @를 붙인 것을 '데코레이터'라고 함(express에서 app.get() 역할)
-  // 데코레이터는 꾸며주는 함수나 클래스와 꼭 붙어있어야 함
+  // appService에 getHi 만들고 적용
   @Get('/hello')
-  // 여기를 띄우면 데코레이터가 인식을 못함
   sayHello(): string {
-    return 'Hello everyone';
+    return this.appService.getHi();
   }
 
-  // Post도 가능
-  @Post('/hi')
-  sayHi(): string {
-    return 'hi hi';
-  }
+  // // @를 붙인 것을 '데코레이터'라고 함(express에서 app.get() 역할)
+  // // 데코레이터는 꾸며주는 함수나 클래스와 꼭 붙어있어야 함
+  // @Get('/hello')
+  // // 여기를 띄우면 데코레이터가 인식을 못함
+  // sayHello(): string {
+  //   return 'Hello everyone';
+  // }
+
+  // // Post도 가능
+  // @Post('/hi')
+  // sayHi(): string {
+  //   return 'hi hi';
+  // }
 }
 // controller는 url을 가져오는 오고 함수를 실행하는 역할
